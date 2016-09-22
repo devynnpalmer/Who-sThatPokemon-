6dcbc2c6148a319b5086eec556603bae5432026b
Devynn R. Palmer <palmerdevynn@gmail.com>
2016-09-21 19:00:21 -0500

    Update ExampleUnitTest.java

--- app/src/test/java/com/devynnpalmer/whosthatpokemon/ExampleUnitTest.java
+++ app/src/test/java/com/devynnpalmer/whosthatpokemon/ExampleUnitTest.java
@@ -4,9 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
+//unit testing!
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
